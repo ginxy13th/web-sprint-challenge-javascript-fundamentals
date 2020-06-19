cc8f606b9710a8a4d936c0313c242d840f7b5be7
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-06-19 13:51:23 -0400

    final commit

--- challenges/closure.js
+++ challenges/closure.js
@@ -23,13 +23,17 @@ myFunction();
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
-// let arr = []
+function summation(num){
+  let arr = []
+  for(let i = 1; i < num + 1; i++){
+  arr.push(i);
+  }
+  return arr.reduce(function(a,b){
+    return a + b
+  }, 0)
 
-// function summation(num){
- 
-// arr.forEach(function(){
+}
 
-// })
-// }
 
-//  console.log(summation(4));
+ console.log(summation(4));
+
